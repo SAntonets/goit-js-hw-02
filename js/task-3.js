b6ffae43dd36fbb77b8b0c3682a4a69fefa773a7
SAntonets <141963865+SAntonets@users.noreply.github.com>
2023-11-17 22:21:46 +0200

    done

--- js/task-3.js
+++ js/task-3.js
@@ -1,13 +1,17 @@
-function getElementWidth(content, padding, border) {
-  const contentWidth = parseFloat(content);
-  const paddingWidth = parseFloat(padding);
-  const borderWidth = parseFloat(border);
-    
-    const totalWidth = contentWidth + paddingWidth * 2 + borderWidth * 2;
-    
-    return totalWidth;
+function checkForSpam(message) {
+  // переведення рядка у нижній регістр
+
+  const lowerCaseString = message.toLowerCase();
+
+  // перевірка на вміст небажаних слів
+   return lowerCaseString.includes("spam") || lowerCaseString.includes("sale");
 }
 
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+// Перевірка роботи функції
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
